e50a11a5696b1df9c90e3699f9d6e4e785a10c11
Mrutyunjaya Behera <mrutyunjaya.9029@gmail.com>
2021-08-05 22:07:46 +0530

    callback chaining

--- app.js
+++ app.js
@@ -2,13 +2,25 @@
 //mapbox API key= pk.eyJ1IjoidmlrYXNoZWUiLCJhIjoiY2tydGNsN284MTEzMTJucWNjMGozd2hkZyJ9.9rua5cLtgfaR98WqOOaSsw
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const chalk = require('chalk');
 
 
-geocode('Mumbai', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-  forecast(data.longitude, data.latitude, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
+const address = process.argv[2];
+if (!address) {
+  console.log(chalk.red('Please provide an address'));
+} else {
+  geocode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    })
   })
-})
+}
+
+console.log(process.argv);
